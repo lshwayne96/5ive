559aafdc8c074b2d428b88fcbcf35621059bc39b
crumpledpaper <ong.junhao.bryan@dhs.sg>
2018-05-14 13:49:07 +0800

    first commit

---
+++ Assets/Scripts/CameraSnapFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSnapFollow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(PlayerStats.room.position.x, PlayerStats.room.position.y, -10f);
+	}
+}
---
+++ Assets/Scripts/PlayerTeleportRoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleportRoom : MonoBehaviour {
+    public Transform targetRoom;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.T)) {
+            transform.position += targetRoom.position - PlayerStats.room.position;
+            //targetRoom = PlayerStats.room;
+        }
+	}
+}
