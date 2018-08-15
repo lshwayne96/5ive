976636be0d13b43350c2aa5c38d2a213b27903e4
Gabriel Yik Ren Jun <gabrielyik@yahoo.com.sg>
2018-08-15 22:39:45 +0800

    no message

---
+++ Assets/T.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class T : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = transform.parent.GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("Called");
+        if (collision.CompareTag("TeleportationBall")) {
+            Debug.Log("Enter");
+            spriteRenderer.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.CompareTag("TeleportationBall")) {
+            Debug.Log("Exit");
+            spriteRenderer.enabled = false;
+        }
+    }
+}
