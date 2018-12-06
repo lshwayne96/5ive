bc11e9f2b2e99ea9b39c3abdab72776037d1265e
GabrielYik <gabrielyik@yahoo.com.sg>
2018-12-07 00:45:11 +0800

    Constants.cs: Make class static

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,4 +1,4 @@
-﻿public class Constants {
+﻿public static class Constants {
     public static readonly float CAMERA_LENGTH = 18f;
     public static readonly float CAMERA_HEIGHT = 10f;
     public static readonly float CAMERA_DEPTH = -10f;
