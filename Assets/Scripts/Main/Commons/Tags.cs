ec9e90950a6ef1b7d70a941908550f6616cf689d
GabrielYik <gabrielyik@yahoo.com.sg>
2019-01-19 14:02:15 +0800

    Add a Tags class to consolidate tags and allow easier name changes

---
+++ Assets/Scripts/Main/Commons/Tags.cs
@@ -0,0 +1,18 @@
+﻿public static class Tags {
+
+	public static readonly string Player = "Player";
+
+	public static readonly string PlayerCamera = "PlayerCamera";
+
+	public static readonly string Ball = "Ball";
+
+	public static readonly string SpecialRoom = "SpecialRoom";
+
+	public static readonly string Menu = "Menu";
+
+	public static readonly string ButtonManager = "ButtonManager";
+
+	public static readonly string NewGameButton = "NewGameButton";
+
+	public static readonly string ComponentManager = "ComponentManager";
+}
