fdf9d888058771907a461e6fde7346b645ca1d9a
GabrielYik <gabrielyik@yahoo.com.sg>
2019-01-20 22:46:24 +0800

    Remove duplicate FileAlreadyExistsMessage class

--- Assets/Scripts/Main/Model/Story/Messages/FileAlreadyExistsMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿/// <summary>
-/// This class represents the message "File Already Exists!".
-/// </summary>
-public class FileAlreadyExistsMessage : IMessage {
-	public string Text {
-		get { return "File Already Exists!"; }
-	}
-}
